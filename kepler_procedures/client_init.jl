4c2e9f75816affcb02e5c87a0f5679e3d414490b
gummiks <gummiks@gmail.com>
2014-03-23 13:09:49 -0400

    fluxerrors added + quality

--- kepler_procedures/client_init.jl
+++ kepler_procedures/client_init.jl
@@ -14,10 +14,10 @@ function get_good_lightcurve_quarters(client, koi_name)
     #
     # PURPOSE:
     #       A function that gets all of the good light curve quarters for a given
-    #       kplr object, with name `koi.name`. It returns an array of time, and flux
+    #       kplr object, with name `koi.name`. It returns an array of time, and flux, and fluxerrors
     #
     # CALLING SEQUENCE:
-    #       time, flux = get_good_lightcurve_quarters(client, koi_name)
+    #       time, flux, fluxerr = get_good_lightcurve_quarters(client, koi_name)
     #
     # INPUTS:
     #       client    - A kplr.api client; the output of client_init()
@@ -26,17 +26,20 @@ function get_good_lightcurve_quarters(client, koi_name)
     # OUTPUTS:
     #       time      - A floating point array, containing the times 
     #       flux      - A floating point array, containing the measured fluxes
+    #       fluxerr   - A floating point array, containing the flux-errors
     #
     # NOTES:
-    #       Need to add ferr, and quality, and only look at the good quality ones
+    #       errors are not scaled yet
     #
     # MODIFICATION HISTORY:
     #       March 22, 2014; by gummiks
+    #       March 23, 2014; by gummiks - note errors are not scaled yet
     #
 
-    time,flux,ferr,quality = [],[];
+    time,pdcsap_flux,pdcsap_flux_err = [],[],[];
     koi = client[:koi](koi_name)
     lcs = koi[:get_light_curves](short_cadence=false)
+
     #iterate over each lightcurve
     for lc in lcs
         #open the lightcurve file for reading
@@ -44,32 +47,50 @@ function get_good_lightcurve_quarters(client, koi_name)
         
         #this hdu contains the data
         hdu_bintable = hdu[2];
-        
-        #hdu_image = hdu[3];
-        
         hdu_bintable_data=hdu_bintable[:data];
         
-        #retrieve the time
-        timetemp=get(hdu_bintable_data, "time");
-        timetemp=convert(Array{Float64,1}, timetemp);
+
+        #------------------------------------------------------------
+        #RETRIEVE DATA
+        #------------------------------------------------------------
+
+        #Time
+        time_temp=get(hdu_bintable_data, "time");
+        time_temp=convert(Array{Float64,1}, time_temp);
         
-        #retrieve the flux
-        fluxtemp=get(hdu_bintable_data, "pdcsap_flux");
-        fluxtemp=convert(Array{Float64,1}, fluxtemp);
+        #Flux - pdcsap_flux
+        pdcsap_flux_temp=get(hdu_bintable_data, "pdcsap_flux");
+        pdcsap_flux_temp=convert(Array{Float64,1}, pdcsap_flux_temp);
+
+        #Flux - pdcsap_flux_err
+        pdcsap_flux_err_temp=get(hdu_bintable_data, "pdcsap_flux_err");
+        pdcsap_flux_err_temp=convert(Array{Float64,1}, pdcsap_flux_err_temp);
+
+        sap_quality_temp=get(hdu_bintable_data, "sap_quality")
+        sap_quality_temp=convert(Array{Float64,1}, sap_quality_temp);
         
-        #strip out bad data
-        ind=get_good_indices(fluxtemp);
-        timetemp=timetemp[ind];
-        fluxtemp=fluxtemp[ind];
+        idx_good = find(x -> x==0, sap_quality_temp)
+        time_temp=time_temp[idx_good];
+        pdcsap_flux_temp=pdcsap_flux_temp[idx_good];
+        pdcsap_flux_err_temp=pdcsap_flux_err_temp[idx_good];
+
+        ##strip out bad data
+        #ind=get_good_indices(pdcsap_flux_temp);
+
+        ##
+        #time_temp=time_temp[ind];
+        #pdcsap_flux_temp=pdcsap_flux_temp[ind];
+        #pdcsap_flux_err_temp=pdcsap_flux_err_temp[ind];
         
-        #normalize each quarter
-        fluxtemp=fluxtemp/mean(fluxtemp);
+        ##normalize each quarter
+        pdcsap_flux_temp=pdcsap_flux_temp/mean(pdcsap_flux_temp);
         
         #build up the combined arrays
-        time=[time,timetemp];
-        flux=[flux,fluxtemp];
+        time=[time,time_temp];
+        pdcsap_flux=[pdcsap_flux,pdcsap_flux_temp];
+        pdcsap_flux_err = [pdcsap_flux_err,pdcsap_flux_err_temp];
 
-        return(time, flux)
+        return(time, pdcsap_flux, pdcsap_flux_err)
     end
 
 end
