d7b25f1a4d91054ceb3a490e90086c7d5893ef89
Guðmundur Kári <gummiks@gmail.com>
2014-04-13 16:28:58 -0400

    Delete test_rms_ser.jl

--- kepler_procedures/test_rms_ser.jl
+++
@@ -1,9 +0,0 @@
-# Big regression test
-function rms_test_ser()
-
-    #Try running the KOI_luncher on a file with 4 koi objects, each in one row
-    #This takes a while in serial
-
-    #Note that we are using the "test" keyword
-    koi_launcher("koi_list.csv", "test");
-end
